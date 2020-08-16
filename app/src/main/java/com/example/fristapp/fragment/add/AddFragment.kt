2b6edcf6088e80682b735ea6b3ab4dfacf46675a
Prakhan Rattakool <prakhan.ra@rmuti.ac.th>
2020-08-16 22:33:49 +0700

    Update fragment

--- app/src/main/java/com/example/fristapp/fragment/add/AddFragment.kt
+++ app/src/main/java/com/example/fristapp/fragment/add/AddFragment.kt
@@ -1,4 +1,4 @@
-package com.example.firstapp.fragments.add
+package com.example.fristapp.fragment.add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
--- app/src/main/java/com/example/fristapp/fragment/list/ListFragment.kt
+++ app/src/main/java/com/example/fristapp/fragment/list/ListFragment.kt
@@ -1,4 +1,4 @@
-package com.example.firstapp.fragments.list
+package com.example.fristapp.fragment.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
