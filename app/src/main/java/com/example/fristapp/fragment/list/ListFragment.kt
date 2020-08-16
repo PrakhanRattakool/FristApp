f91b9177c6e4378d334147d618e974f8fd2be2a9
Prakhan Rattakool <prakhan.ra@rmuti.ac.th>
2020-08-16 22:46:43 +0700

    Update Addfragment & Listfragment

--- app/src/main/java/com/example/fristapp/fragment/list/ListFragment.kt
+++ app/src/main/java/com/example/fristapp/fragment/list/ListFragment.kt
@@ -1,20 +1,51 @@
 package com.example.fristapp.fragment.list
 
+import UserViewModel
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.fristapp.R
+import com.example.fristapp.database.User
+import kotlinx.android.synthetic.main.fragment_add.*
+import kotlinx.android.synthetic.main.fragment_add.view.*
 
-class ListFragment : Fragment() {
+class AddFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    private lateinit var mUserViewModel : UserViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        view.add_btn.setOnClickListener {
+            insertDataToDatabase()
+        }
+        return view
     }
+    private fun insertDataToDatabase(){
+        val firstName = addfirstName.text.toString()
+        val Phone = addphoneNumber.text
 
+        if(inputCheck(firstName,Phone)) {
+            val user = User(0, firstName, Integer.parseInt(Phone.toString()))
+
+            mUserViewModel.addUser(user)
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_LONG).show()
+
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }else{
+            Toast.makeText(requireContext(),"Please fill out all fields.", Toast.LENGTH_LONG).show()
+        }
+
+    }
+    private fun inputCheck(firstName: String,Phone: Editable): Boolean{
+        return !(TextUtils.isEmpty(firstName) && Phone.isEmpty())
+    }
 }
