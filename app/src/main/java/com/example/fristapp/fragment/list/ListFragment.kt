c79b026c37204ca4c2eb7a7405add9727630d8bc
Prakhan Rattakool <prakhan.ra@rmuti.ac.th>
2020-08-16 23:21:25 +0700

    Fix Bug Manu

--- app/src/main/java/com/example/fristapp/fragment/list/ListFragment.kt
+++ app/src/main/java/com/example/fristapp/fragment/list/ListFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.fristapp.R
+import kotlinx.android.synthetic.main.fragment_list.view.*
 
-class ListFragment : Fragment() {
+class  ListFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }
+        return view
     }
-
 }
