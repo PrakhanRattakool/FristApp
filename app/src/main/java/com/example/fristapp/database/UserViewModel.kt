0fb5c9b0a0d9a1aa29069a1e399be4b040a8bde9
Prakhan Rattakool <prakhan.ra@rmuti.ac.th>
2020-08-16 21:37:27 +0700

    Fix UserViewModel.kt

--- app/src/main/java/com/example/fristapp/database/UserViewModel.kt
+++ app/src/main/java/com/example/fristapp/database/UserViewModel.kt
@@ -1,25 +1,21 @@
-package com.example.firstapp.Database
-
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
+import com.example.firstapp.Data.UserRepository
+import com.example.firstapp.Database.UserDatabase
 import com.example.fristapp.database.User
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
 class UserViewModel(application: Application): AndroidViewModel(application) {
-
     private val readAllData: LiveData<List<User>>
     private val repository: UserRepository
-
     init {
         val userDao = UserDatabase.getDatabase(application).userDao()
         repository = UserRepository(userDao)
-        readAllData = repository.readAllDate
+        readAllData = repository.readAllData
     }
-
-    fun addUser(user: User){
+    fun addUser(user: User) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.addUser(user)
         }
